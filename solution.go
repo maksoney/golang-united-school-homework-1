ed6b8752733d61700e8ae5dc16a9f0a22c92e129
Serjeant Maksoney <99703153+maksoney@users.noreply.github.com>
2022-02-17 06:54:05 +0500

    first solution

--- solution.go
+++ solution.go
@@ -1,5 +1,12 @@
-package solution
+package main
 
-func GetMessage() string {
-	return ""
+import (
+	"fmt"
+
+	"github.com/kyokomi/emoji/v2"
+)
+
+func main() {
+	world_map := emoji.Sprint("Hello :world_map:!")
+	fmt.Println(world_map)
 }
